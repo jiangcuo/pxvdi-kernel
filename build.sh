ed2b7e5fd1bc96e43c239f8c150094eade762715
jiangcuo <jiangcuo@bingsin.com>
2026-06-13 19:19:16 +0800

    更新构建

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-tag="138.0.0"
-url="https://atomgit.com/openeuler/kernel"
+source "$DIR/env.sh"
+
 ARCH=$1
 kertag="${tag%%.*}"
 
@@ -13,7 +13,7 @@ if [ ! -d  "$DIR/linux" ]; then
     git clone $url -b 6.6.0-$tag --depth=1  "$DIR/linux"
 fi
 
-rm -rf build 
+rm -rf build
 
 rsync -a linux/ build
 
@@ -22,17 +22,20 @@ case "$ARCH" in
         DEFCONFIG="openeuler_defconfig"
         CROSS_COMPILE="x86_64-linux-gnu-"
         ARCH=x86_64
+        DEB_ARCH=amd64
     ;;
     "aarch64"|"arm64")
         DEFCONFIG="openeuler_defconfig"
         CROSS_COMPILE="aarch64-linux-gnu-"
         ARCH=arm64
+        DEB_ARCH=arm64
     ;;
     "loongarch64"|"loong64")
         DEFCONFIG="loongson3_defconfig"
         CROSS_COMPILE="loongarch64-linux-gnu-"
         ARCH=loongarch
-    ;; 
+        DEB_ARCH=loong64
+    ;;
     *)
     echo "Unsupported architecture: $ARCH"
     exit 1
@@ -53,9 +56,33 @@ build(){
     ARCH="$ARCH" ./scripts/kconfig/merge_config.sh -m .config  $DIR/config/$ARCH.kconfig
     ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" make olddefconfig
 
-    ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" make LOCALVERSION="" bindeb-pkg -j $(nproc) 
+    ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" make LOCALVERSION="" bindeb-pkg -j $(nproc)
+}
+
+build_meta(){
+    local kernel_ver="6.6.${kertag}-pxvdi"
+    local meta_name="linux-image-6.6-pxvdi"
+    local tmpdir="$DIR/.meta-pkg-tmp"
+
+    rm -rf "$tmpdir"
+    mkdir -p "$tmpdir/DEBIAN"
+
+    cat > "$tmpdir/DEBIAN/control" << EOF
+Package: ${meta_name}
+Version: ${tag}
+Architecture: ${DEB_ARCH}
+Maintainer: pxvdi
+Depends: linux-image-${kernel_ver}
+Section: metapackages
+Priority: optional
+Description: pxvdi kernel meta package
+ Meta package pointing to the pxvdi custom kernel ${kernel_ver}.
+EOF
+
+    dpkg-deb --build "$tmpdir" "$DIR/${meta_name}_${tag}_${DEB_ARCH}.deb"
+    rm -rf "$tmpdir"
+    echo "Meta package: ${meta_name}_${tag}_${DEB_ARCH}.deb"
 }
 
 build
-
-
+build_meta
---
+++ env.sh
@@ -0,0 +1,2 @@
+tag="158.0.0"
+url="https://atomgit.com/openeuler/kernel"
